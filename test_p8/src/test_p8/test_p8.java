9af2b489486e50127720fa2c0d589e82b9adb1b5
kevin86930 <kevin3234747@gmail.com>
2018-11-28 22:59:31 +0800

    first commit

---
+++ test_p8/src/test_p8/test_p8.java
@@ -0,0 +1,15 @@
+
+package test_p8;
+import test_p8.Car;
+
+public class test_p8 {
+	
+		public static void main(String[] args)
+		{
+			Car car1 ;
+			car1 = new Car();
+			car1.show();
+			}
+	}
+
+
